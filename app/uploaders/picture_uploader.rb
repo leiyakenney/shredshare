b02a19c9792d604a6bdfe8b8338b731cd4f9a62d
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-23 12:44:53 -0600

    Add styling and picture uploader

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,11 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
