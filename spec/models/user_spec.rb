31f941301cc99244a8150ddf4e70200dac9377f3
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-23 12:34:54 -0600

    Add user model and spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    it {should validate_presence_of(:user_name)}
+    it {should validate_uniqueness_of(:user_name)}
+		it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+  end
+end
