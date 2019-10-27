29af9a1fba77a31a9138d668b593d43aae5a5c01
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-27 16:59:56 -0600

    create route for ride dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/ride_dashboard', to: "ride/sessions#new"
   # devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :users
 
   root to: 'welcome#index'
+
 end
