7af8e50ce4923f6cede660fc8cfe6802c56d57bc
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-27 15:39:19 -0600

    adds /profile path to users#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
-  resources :users
+  resources :users, only: :show, as: :profile
 
   root to: 'welcome#index'
 end
