a1355b869d851da72aaed111036284af48da2ccb
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-27 17:46:35 -0600

    Update route name to be consistant with MVC conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/ride_dashboard', to: "ride/sessions#new"
+
+  get '/trip_dashboard', to: "trips/trips#new"
+
   # devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
