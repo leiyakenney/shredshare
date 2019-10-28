71d44475dfeddf3510bfb3052f3323dc58f862bb
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-27 18:33:32 -0600

    current trip method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_trip
-    @trip || Trip.new(session[:trip]) ||= Hash.new(0)
+    @trip || Trip.new(session[:trip])
   end
 
 protected
