3074f75c548c9926dca5872e0f8a7d2e173383a0
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-28 16:03:43 -0600

    Adds a has_many relationship between user and reviews with a dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :picture, PictureUploader
+
+  has_many :reviews, :dependent => :destroy
+
   validates_presence_of :user_name, :first_name, :last_name, :email
   validates_uniqueness_of :user_name
-  
+
 end
