1c4dc5fc9eb9ed1aeb0d570a3bb7562636e6f7a8
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-28 11:47:30 -0600

    Update test

--- spec/features/user/can_create_a_trip_spec.rb
+++ spec/features/user/can_create_a_trip_spec.rb
@@ -1,17 +1,4 @@
 require 'rails_helper'
-# As a logged in user, when I visit '/dashboard' and I click on 'I Need Cheeks In My Seats (Give A Ride)',
-# I should be taken to '/dashboard/drivers/new'
-# I should see a form to fill in information to create a new trip.
-# The following fields will be required to successfully create a new trip:
-#
-# Ride Offer (drop down menu to one-way or round-trip)
-# Seats Available (drop down menu)
-# Gear Storage Type (drop down menu)
-# AWD or 4x4 (checkbox)
-# Date
-# Where I Am Going
-# Send It! (button)
-# The newly created trip should appear on the rides index page.
 
 RSpec.describe "A driver can create a trip" do
   describe "Putting cheeks in your seats" do
@@ -25,9 +12,22 @@ RSpec.describe "A driver can create a trip" do
                                   sport_type: "ski",
                                   email: "jerry@email.com")
     end
-    it "I can click on my link" do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
 
+    it "As a logged in user, when I visit '/trip_dashboard' and I click on
+     'I Need Cheeks In My Seats (Give A Ride)', I should be taken to
+      '/dashboard/drivers/new' I should see a form to fill in information
+      to create a new trip. The following fields will be required to
+      successfully create a new trip:
+      - Ride Offer (drop down menu to one-way or round-trip)
+      - Seats Available (drop down menu)
+      - Gear Storage Type (drop down menu)
+      - AWD or 4x4 (checkbox)
+      - Date
+      - Where I Am Going
+      - Send It! (button)
+       The newly created trip should appear on the rides index page." do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
+      
     end
   end
 end
