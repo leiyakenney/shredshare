89785ebe9fffb2d517a8af4587ca305a3e28b5c4
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-27 21:17:06 -0600

    Create new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get '/trip_dashboard', to: "trips/trips#new"
+  get '/trip_filter', to: "trips/filter#new"
+  post '/trips', to: "trips/filter#create", as: :trip_filter_creation
+
 
   # devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
