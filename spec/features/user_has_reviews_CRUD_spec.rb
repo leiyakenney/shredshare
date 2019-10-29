7b648844282d314eb218134da822731945663a44
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-29 15:43:44 -0600

    Updates test to properly create @user

--- spec/features/user_has_reviews_CRUD_spec.rb
+++ spec/features/user_has_reviews_CRUD_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "as a user" do
   before :each do
-    @user_1 = User.create!(user_name: "jerry",
+    @user = User.create!(user_name: "jerry",
               first_name: "Jerry",
               last_name: "Jones",
               pass: "Epic",
@@ -12,22 +12,14 @@ describe "as a user" do
               password: "password",
               password_confirmation: "password")
 
-    @user_2 = User.create!(user_name: "jerralina",
-              first_name: "Jerralina",
-              last_name: "Jones",
-              pass: "Epic",
-              bio: "I sent it",
-              sport_type: "ski",
-              email: "jerralina@email.com",
-              password: "password",
-              password_confirmation: "password")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
 
   it "I can have a list of reviews" do
-    review_1 = @user_1.reviews.create!(title: "Full send brah", content: "Jerry's rad.", rating: 5)
-    review_2 = @user_1.reviews.create!(title: "Crazy driver", content: "Jerry goes full send on the roads. Scary.", rating: 2)
+    review_1 = @user.reviews.create!(title: "Full send brah", content: "Jerry's rad.", rating: 5)
+    review_2 = @user.reviews.create!(title: "Crazy driver", content: "Jerry goes full send on the roads. Scary.", rating: 2)
 
-    visit profile_path(@user_1)
+    visit profile_path(@user)
 
     within "review-#{review_1.id}" do
       expect(page).to have_content(review_1.title)
@@ -41,7 +33,7 @@ describe "as a user" do
       expect(page).to have_content("User Rating: #{review_2.rating}/5")
     end
 
-    expect(@user_1.avg_rtg).to eq(3.5)
-    expect(page).to have_content("Average User Rating: #{@user_1.avg_rtg}/5")
+    expect(@user.avg_rtg).to eq(3.5)
+    expect(page).to have_content("Average User Rating: #{@user.avg_rtg}/5")
   end
 end
