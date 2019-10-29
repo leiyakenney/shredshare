c59abd60470fdfea51539f153240f3e3ed7332ac
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-29 10:32:00 -0600

    Create test shell for beginning options for AR form creations and results with single choice selected

---
+++ spec/features/user/passenger/can_filter_trips_they_see_spec.rb
@@ -0,0 +1,104 @@
+# As a passenger when i fill out the need a ride form, and select the fields i
+# want and click on 'SEND IT', I am taken to the ride index page.
+
+# On the ride index page, i see only rides for the upcoming week that match my
+# specified filters.
+
+# Next to each ride, i see a button or link to 'Catch This Lift'
+
+  # Trip Type
+
+require 'rails_helper'
+
+RSpec.decribe "Filtered views for passengers" do
+  describe "Using the filter form to build AR queries" do
+    before :each do
+
+    end
+
+    it "As a passenger I leave all form options blank" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - All Dates" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Monday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Tuesday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Wednesday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Thursday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Friday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Saturday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Sunday" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Desired Dates - Weekend" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Pass Type - No Preference" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Pass Type - Other" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Pass Type - Epic" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Pass Type - IKON" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Pass Type - No Pass" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Recreation Type - Other" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Recreation Type - Ski" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Recreation Type - Board" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Recreation Type - Both" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Trip Type - One Way" do
+
+    end
+
+    it "As a passenger I leave all form options blank except Trip Type - Round Trip" do
+
+    end
+    
+  end
+end
