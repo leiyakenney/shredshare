f9c20016be211444b2035ed602655ae9ced7079e
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-30 10:18:16 -0600

    configures User model to use Devise provided tools for OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers => [:google_oauth2
+         ]
   mount_uploader :picture, PictureUploader
+
   validates_presence_of :user_name, :first_name, :last_name, :email
   validates_uniqueness_of :user_name
 
   has_many :vehicles
+
 
 end
