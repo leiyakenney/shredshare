30295f81cd965a2daa39fa96956614f10a55edfd
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-30 11:59:56 -0600

    removes binding.pry

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      binding.pry
       sign_in @user
       redirect_to user_path(@user.id), notice: 'Successfully logged in using your Google account.'
     else
