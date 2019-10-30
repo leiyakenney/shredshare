3a5429a500911b2ccf482b149fae5bb83187344f
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-30 13:28:50 -0600

    Remove subject line in service test

--- spec/services/rtd_locations_service_spec.rb
+++ spec/services/rtd_locations_service_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 describe RtdLocationsService do
-  subject = RtdLocationsService.new
   context "instance methods" do
     it "returns all rtd locations in Denver Metro Area" do
 
-      park_n_rides = subject.rtd_locations
+      park_n_rides = RtdLocationsService.rtd_locations
       parsed_data = JSON.parse(park_n_rides.body, symbolize_names: true)
       expect(parsed_data).to be_a Hash
       expect(parsed_data[:"Denver,CO"]).to be_an Array
