ab184d64430f66f134d7e410fd20586bfc411875
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-31 17:02:44 -0600

    Update Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,14 +2,12 @@ class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
   belongs_to :rtd_location
+  has_many :rides, :dependent => :delete_all
 
   validates_presence_of :seats_available
   validates_presence_of :vehicle_id
   validates_presence_of :rtd_location_id
   validates_presence_of :destination_point
-  validates_presence_of :day_of_trip
-  validates_presence_of :month_of_trip
-  validates_presence_of :year_of_trip
   validates_presence_of :ride_type
 
 end
