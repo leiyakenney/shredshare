b5a88da3177bb232888dda92ee2b904faf7da63f
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-01 00:21:32 -0600

    Updates reviews controller to include create method plus private review_params
    Co-Authored-By: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,5 +7,21 @@ class ReviewsController<ApplicationController
 
   def new
     @review = @user.reviews.new
-  end 
+  end
+
+  def create
+    @review = @user.reviews.create(review_params)
+    if @review.save
+      redirect_to "/users/#{@user.id}", notice: 'You have successfully reviewed this lift buddy!'
+    else
+      redirect_to new_user_review_path(@user), alert: "Please fill in all fields in order to create a review."
+    end
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating)
+  end
 end
