b55621fc1ea85dda5a9535b1e5c40b90751ecf34
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-31 23:31:48 -0600

    Remove filter content

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,6 @@ Rails.application.routes.draw do
 
   get '/trip_dashboard', to: "trips/buttons#index"
 
-  get '/trip_filter', to: "trips/filter#new"
-  post '/trips', to: "trips/filter#create", as: :trip_filter_creation
-
   get '/trip_dashboard/drivers/new', to: "trips/trips#new", as: :trip_creation
   post '/trip_dashboard/drivers/create', to: "trips/trips#create", as: :trip_create
   get '/trip_dashboard/:id', to: "trips/trips#show", as: :trip_show
